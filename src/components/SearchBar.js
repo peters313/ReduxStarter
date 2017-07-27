e6aa4d8220e581608f3481d65455cd5d652477c9
Eric Peters <eric.peters3@gmail.com>
2017-07-26 23:45:21 -0500

    initial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { searchTerm: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.searchTerm}
+                    onChange={event => this.setState({ searchTerm: event.target.value })} />
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDQics2207349zvO9qUlH5O-V1_OeN1mNQ';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
