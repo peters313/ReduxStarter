e224242ed1598a32148163b9042d40a0304c0c0d
Eric Peters <eric.peters3@gmail.com>
2017-07-28 21:50:21 -0500

    initial commit

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import VideoListItem from "./VideoListItem"
 
+
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
         return <VideoListItem key={video.etag} video={video} />
